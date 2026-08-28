c915c4dfd85c6e00995b0d37e6bf38d9c52ccbee
Darwin pc <nocorreoxd@gmail.xd>
2026-08-28 19:01:19 +0200

    Mini avanze

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,13 @@ import TarjetaInfo from "@/app/components/intro"; // El @ indica que comienza a 
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <div >
+      <main >
         <TarjetaInfo  // Nombre de la tarjeta 
         titulo="¡Hola, futuro programador!"  // Son los parametros
         subtitulo="Este es tu espacio centralizado de estudio."
         texto="Aquí encontrarás todos los apuntes organizados y actualizados que voy creando durante mi aprendizaje de Next.js, React y el ecosistema frontend. He utilizado el App Router para estructurar todo de forma intuitiva. ¡Espero que te sirvan de guía tanto como a mí crearlos!"
+        img="/file.svg"
       />
         </main>
     </div>
