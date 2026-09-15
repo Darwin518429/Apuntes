3da04211c2e79a2ba1f95c2da768db2041b587e6
Darwin <d.biag@sapalomera.cat>
2026-09-15 14:16:37 +0200

    Intento2

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
     output: 'export',
-  images: { unoptimized: true },
+  //images: { unoptimized: true },
   basePath: '/Apuntes',
 };
 
